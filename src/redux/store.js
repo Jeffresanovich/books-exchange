9822be3c428a6055fe73259a13e5b6867fc36f22
jeffresanovich <jeffresanovich@gmail.com>
2023-09-25 08:06:21 -0300

    userSlice delete

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import bookSlice from "./slice/bookSlice";
-import userSlice from "./slice/userSlice";
 
 export const store = configureStore({
-  reducer: { bookSlice, userSlice },
+  reducer: { bookSlice },
 });
