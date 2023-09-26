7126714cc0f7fcc318e7f1d0a27fa5ad0d192465
jeffresanovich <jeffresanovich@gmail.com>
2023-09-26 19:15:33 -0300

    model db

---
+++ src/data/DB.js
@@ -0,0 +1,40 @@
+const book = [
+  {
+    id: 0,
+    title: "string",
+    authors: ["string", "string"],
+    edition: "string",
+    synopsis: "string",
+    image: ["string", "string"],
+    pages: 0,
+    language: "string",
+    publisher: "string",
+    date_published: "2017-08-29T00:00:00.000Z",
+    subjects: ["string", "string"],
+    current_user: {
+      user_name: "string",
+      initial_date: "2017-08-29T00:00:00.000Z",
+      final_date: "2017-08-29T00:00:00.000Z",
+    },
+    previous_users: [
+      {
+        user_name: "string",
+        date: "2017-08-29T00:00:00.000Z",
+        rating: 0,
+        comment: "string",
+      },
+    ],
+  },
+];
+
+const user = [
+  {
+    user_name: "string",
+    name: "string",
+    last_name: "string",
+    age: "string",
+    email: "string",
+    favorite_genre: "string",
+    page_xday: 0,
+  },
+];
