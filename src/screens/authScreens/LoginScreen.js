7b962ef986efb332a0150e320b9ac472ae9bcef0
Jeffrey <jeffresanovich@gmail.com>
2023-09-30 20:06:19 -0300

    loading component

--- src/screens/authScreens/LoginScreen.js
+++ src/screens/authScreens/LoginScreen.js
@@ -12,31 +12,35 @@ import { firebase_auth } from "../../firebase/authFirebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 
 import { useDispatch } from "react-redux";
-import { setUser, setIdToken, clearUser } from "../../redux/slice/authSlice";
+import { setUser, setIdToken } from "../../redux/slice/authSlice";
+import { LoadingComponent } from "../../components/LoadingComponent";
 
 import { themeColors } from "../../theme/commonStyles";
 
 const LoginScreen = ({ navigation }) => {
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
     try {
+      setIsLoading(true);
       const response = await signInWithEmailAndPassword(
         firebase_auth,
         email,
         password
       );
 
-      //console.log(response);
       dispatch(setUser(response.user.email));
       dispatch(setIdToken(response._tokenResponse.idToken));
+      if (response.user.email) setIsLoading(false);
     } catch (error) {
       console.log("LoginError: " + error.message);
     }
   };
 
+  console.log(isLoading);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Inicio de Sesión</Text>
@@ -58,9 +62,6 @@ const LoginScreen = ({ navigation }) => {
       </TouchableOpacity>
       <Pressable onPress={() => navigation.navigate("Register")}>
         <Text style={styles.registroText}>No tienes cuenta? Registrate</Text>
-      </Pressable>
-      <Pressable onPress={() => dispatch(clearUser())}>
-        <Text style={styles.registroText}>Salir</Text>
       </Pressable>
     </View>
   );
