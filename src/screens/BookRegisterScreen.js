6af36f0d59bda8088ee1495a842024ce7de9713c
jeffresanovich <jeffresanovich@gmail.com>
2023-10-10 14:34:50 -0300

    bookregisterscreen created

---
+++ src/screens/BookRegisterScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const BookRegisterScreen = () => {
+  return (
+    <View>
+      <Text>BookRegisterScreen</Text>
+    </View>
+  );
+};
+
+export default BookRegisterScreen;
+
+const styles = StyleSheet.create({});
